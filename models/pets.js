78e855c9d02ec1b9c8f6f22746284ae6f0ba0aa0
dean-pound <dean.pound@onthebeach.co.uk>
2019-10-03 18:23:28 +0100

    Implements getPetsByOwnerID model

--- models/pets.js
+++ models/pets.js
@@ -1,12 +1,22 @@
 const { readFile, readdir } = require('fs').promises;
 
 const fetchPetByID = (petID, cb) => {
-  return readFile(`./data/pets/${petID}.json`, 'utf-8').then((petsJSON) => {
-    return JSON.parse(petsJSON);
-  });
-};
+  return readFile(`./data/pets/${petID}.json`, 'utf-8').then(JSON.parse);
+  };
 
-const fetchPetsByOwnerId = (ownerID) => {};
+const fetchPetsByOwnerId = (ownerID) => {
+  return readdir(`./data/pets`)
+  .then(petsAll => {
+    return Promise.all(
+      petsAll.map(pet => {
+        const [petID] = pet.match(/(p\d+)/);
+        return fetchPetByID(petID);
+      })
+    );
+  }).then(pets => {
+    return pets.filter(({owner}) => owner === ownerID)
+  })
+};
 
 module.exports = {
   fetchPetByID,
