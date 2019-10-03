584f159ea9af781ab14a8d3c8df8a3c2c2d8b832
dean-pound <dean.pound@onthebeach.co.uk>
2019-10-03 12:39:36 +0100

    Complete getOwnerByID tests for controller and model

--- controllers/owners.js
+++ controllers/owners.js
@@ -1,7 +1,20 @@
 const { fetchAllOwners, fetchOwnerById } = require('../models/owners');
 
-const getOwners = (request, response) => {};
+const getOwners = (request, response) => {
+  fetchAllOwners().then((owners) => {
+    response.write(JSON.stringify({ owners }));
+    response.setHeader('content-type', 'application/json');
+    response.end();
+  });
+};
 
-const getOwnerByID = (request, response) => {};
+const getOwnerByID = (request, response) => {
+  const [, ownerID] = request.url.match(/\/api\/owners\/(o\d+)/);
+  fetchOwnerById(ownerID).then((owner) => {
+    response.write(JSON.stringify({ owner }));
+    response.setHeader('content-type', 'application/json');
+    response.end();
+  });
+};
 
 module.exports = { getOwners, getOwnerByID };
--- models/owners.js
+++ models/owners.js
@@ -2,7 +2,11 @@ const { readFile, readdir } = require('fs').promises;
 
 const fetchAllOwners = () => {};
 
-const fetchOwnerById = (ownerID) => {};
+const fetchOwnerById = (ownerID) => {
+  return readFile(`./data/owners/${ownerID}.json`, 'utf-8').then((ownerJSON) => {
+    return JSON.parse(ownerJSON);
+  });
+};
 
 module.exports = {
   fetchAllOwners,
