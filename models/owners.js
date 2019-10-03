ce24480b58625ea983053ced5eabc7bcd408164c
dean-pound <dean.pound@onthebeach.co.uk>
2019-10-03 14:25:19 +0100

    Implemented fetchAllOwners function in ./models

--- models/owners.js
+++ models/owners.js
@@ -1,6 +1,14 @@
 const { readFile, readdir } = require('fs').promises;
 
-const fetchAllOwners = () => {};
+const fetchAllOwners = () => {
+  return readdir(`./data/owners/`, 'utf-8').then((owners) => {
+    return Promise.all(
+      owners.map((owner) => {
+        return fetchOwnerById(owner);
+      })
+    );
+  });
+};
 
 const fetchOwnerById = (ownerID) => {
   return readFile(`./data/owners/${ownerID}.json`, 'utf-8').then((ownerJSON) => {
